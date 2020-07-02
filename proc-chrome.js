de220b4bfe8de0f218fd7021ec1255f0e955e80b
Max Leitch <mjleitch@gmail.com>
2020-07-01 18:11:43 -0600

    Update proc-chrome.js

--- proc-chrome.js
+++ proc-chrome.js
@@ -1,26 +1,23 @@
-export default (async (url='http://example.com/', port=9222, timeout=200000)=>{
-  
-  const process = Deno.run({
-    cmd: [
-      //'google-chrome',
-      //'chrome-browser',
-      '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome',
-      '--headless',
-      '--remote-debugging-port='+port,
-      url,
-    ],
-    stdout: 'piped',
-    stderr: 'piped',
-  });
+export default (async (url = 'http://example.com/', port = 9222, timeout = 200000) => {
 
-  const buff = new Uint8Array(255);
-  await process.stderr.read(buff);
-  const message = new TextDecoder().decode(buff);
-  const wsUrl = message.slice(23);
+    const process = Deno.run({
+        cmd: [
+            //'google-chrome',
+            //'chrome-browser',
+            '/Applications/Chromium.app/Contents/MacOS/Chromium',
+            '--remote-debugging-port=' + port,
+            url,
+        ],
+        stdout: 'piped',
+        stderr: 'piped',
+    });
 
-  setTimeout(()=> process.close(), timeout);
-  
-  return [process, wsUrl];
+    const buff = new Uint8Array(255);
+    await process.stderr.read(buff);
+    const message = new TextDecoder().decode(buff);
+    const wsUrl = message.slice(23);
+
+    setTimeout(() => process.close(), timeout);
+
+    return [process, wsUrl];
 });
-
-
